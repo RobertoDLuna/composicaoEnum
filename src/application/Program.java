3152ec3d006eb3793f1ec85eef6f0e5417bf830f
Roberto <robertocgw@gmail.com>
2024-08-02 14:54:05 -0300

    criada a classe Client no pacote entities

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,23 +1,23 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Locale;
 import java.util.Scanner;
 
-import entities.OrderItem;
-import entities.Product;
+import entities.Client;
 
 public class Program {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		Product p = new Product("TV", 1000.00);
+		Client cli = new Client("Robs", "roberto@email.com", sdf.parse("20/10/1992"));
 		
-		OrderItem oi1 = new OrderItem(1, 1000.00, p);
-		
-		System.out.println(oi1);
-		
+		System.out.println(cli);
 		
 		sc.close();
 	}
