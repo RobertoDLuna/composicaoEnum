67b01f186b4237fcd99e6ffa413ecf35c45af1bd
Roberto <robertocgw@gmail.com>
2024-08-02 14:35:24 -0300

    adicionado o metodo toString para impressão dos dados de OrderItem e algumas configurações na classe Program

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,24 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 import entities.OrderItem;
 import entities.Product;
 
 public class Program {
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
 		Product p = new Product("TV", 1000.00);
 		
 		OrderItem oi1 = new OrderItem(1, 1000.00, p);
 		
-		System.out.println(oi1.getProduct().getName());
+		System.out.println(oi1);
 		
+		
+		sc.close();
 	}
 }
